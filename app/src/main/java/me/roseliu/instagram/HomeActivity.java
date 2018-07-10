03a5f76d64244916a63a74da1e79ffc760e49341
Rose Liu <roseliu@fb.com>
2018-07-10 16:07:39 -0700

    sign up, log out

--- app/src/main/java/me/roseliu/instagram/HomeActivity.java
+++ app/src/main/java/me/roseliu/instagram/HomeActivity.java
@@ -1,12 +1,17 @@
 package me.roseliu.instagram;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import java.util.List;
 
@@ -22,9 +27,9 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setElevation(
-                getResources().getDimensionPixelSize(R.dimen.action_bar_elevation)
-        );
+//        getSupportActionBar().setElevation(
+//                getResources().getDimensionPixelSize(R.dimen.action_bar_elevation)
+//        );
 
 
         final Post.Query postsQuery = new Post.Query();
@@ -45,5 +50,25 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         });
+
+        Button btLogout = findViewById(R.id.btLogout);
+        btLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                final Intent intent = new Intent(HomeActivity.this, LoginActivity.class); //since inside a callback
+                startActivity(intent);
+            }
+        });
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
 }
--- app/src/main/java/me/roseliu/instagram/LoginActivity.java
+++ app/src/main/java/me/roseliu/instagram/LoginActivity.java
@@ -11,12 +11,14 @@ import android.widget.EditText;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText usernameInput;
     private EditText passwordInput;
     private Button btLogin;
+    private Button btSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +28,29 @@ public class LoginActivity extends AppCompatActivity {
         usernameInput = findViewById(R.id.etUsername);
         passwordInput = findViewById(R.id.etPassword);
         btLogin = findViewById(R.id.btLogin);
+        btSignup = findViewById(R.id.btSignup);
+
+        if(ParseUser.getCurrentUser()!=null){
+            final Intent intent = new Intent(LoginActivity.this, HomeActivity.class); //since inside a callback
+            startActivity(intent);
+        }
 
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final String username = usernameInput.getText().toString();
                 final String password = passwordInput.getText().toString();
-                login(username,password);
+                login(username, password);
+
+            }
+        });
+
+        btSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = usernameInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+                signUp(username, password);
 
             }
         });
@@ -41,22 +59,44 @@ public class LoginActivity extends AppCompatActivity {
 
     //don't use the log in function that runs on main thread
 
-    private void login(String username, String password){
+    private void login(String username, String password) {
         ParseUser.logInInBackground(username, password, new LogInCallback() {
             @Override
             public void done(ParseUser user, ParseException e) {
 
-                if (e == null){
+                if (e == null) {
                     Log.d("LoginActivity", "Login Successful");
                     final Intent intent = new Intent(LoginActivity.this, HomeActivity.class); //since inside a callback
                     startActivity(intent);
                     finish();
-                }
-                else {
+
+                } else {
                     Log.d("LoginActivity", "Login Failure");
                     e.printStackTrace();
                 }
             }
+        });
+    }
+
+    private void signUp(String username, String password) {
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("LoginActivity", "Sign up Successful");
+                    final Intent intent = new Intent(LoginActivity.this, HomeActivity.class); //since inside a callback
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Log.d("LoginActivity", "Sign up Failure");
+                    e.printStackTrace();
+                }
+            }
+
         });
     }
 }
